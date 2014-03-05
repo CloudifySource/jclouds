db6a0dcc7b17dd96668e4b64072aeca516e62079
iliapolo <elip@gigaspaces.com>
2014-03-05 19:19:36 +0200

    Refactor and optimize tests

--- providers/softlayer/src/test/java/org/jclouds/softlayer/features/guest/VirtualGuestClientLiveTest.java
+++ providers/softlayer/src/test/java/org/jclouds/softlayer/features/guest/VirtualGuestClientLiveTest.java
@@ -16,41 +16,25 @@
  */
 package org.jclouds.softlayer.features.guest;
 
-import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSet.Builder;
-import com.google.common.collect.Iterables;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
 import com.google.inject.TypeLiteral;
-import org.jclouds.compute.domain.Template;
-import org.jclouds.compute.domain.TemplateBuilder;
 import org.jclouds.softlayer.SoftLayerClient;
 import org.jclouds.softlayer.domain.guest.VirtualGuest;
-import org.jclouds.softlayer.domain.product.ProductItem;
 import org.jclouds.softlayer.domain.product.ProductItemPrice;
 import org.jclouds.softlayer.domain.product.ProductOrder;
 import org.jclouds.softlayer.domain.product.ProductOrderReceipt;
-import org.jclouds.softlayer.domain.product.ProductPackage;
-import org.jclouds.softlayer.domain.server.HardwareServer;
 import org.jclouds.softlayer.features.BaseSoftLayerClientLiveTest;
-import org.jclouds.softlayer.features.ProductPackageClientLiveTest;
 import org.testng.annotations.Test;
 
 import java.util.Properties;
 import java.util.Random;
 import java.util.Set;
 
-import static com.google.common.base.Predicates.and;
-import static com.google.common.collect.Iterables.find;
-import static com.google.common.collect.Iterables.get;
-import static org.jclouds.softlayer.predicates.ProductItemPredicates.capacity;
-import static org.jclouds.softlayer.predicates.ProductItemPredicates.categoryCode;
-import static org.jclouds.softlayer.predicates.ProductPackagePredicates.named;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 /**
  * Tests behavior of {@code VirtualGuestClient}
@@ -61,7 +45,7 @@ import static org.testng.Assert.assertTrue;
 public class VirtualGuestClientLiveTest extends BaseSoftLayerClientLiveTest {
 
    private static final String TEST_HOSTNAME_PREFIX = "livetest";
-   private TemplateBuilder templateBuilder;
+   private static final int PACKAGE_ID = 46;
 
    @Test
    public void testListVirtualGuests() throws Exception {
@@ -77,8 +61,6 @@ public class VirtualGuestClientLiveTest extends BaseSoftLayerClientLiveTest {
 
    @Test(groups = "live")
    public void testCancelAndPlaceOrder() {
-      // TODO: Should also check if there are active transactions before trying to cancel.
-      // objectMask: virtualGuests.activeTransaction
       for (VirtualGuest guest : api().listVirtualGuests()) {
          if (guest.getHostname().startsWith(TEST_HOSTNAME_PREFIX)) {
             if (guest.getBillingItemId() != -1) {
@@ -87,21 +69,15 @@ public class VirtualGuestClientLiveTest extends BaseSoftLayerClientLiveTest {
          }
       }
 
-      int pkgId = Iterables.find(api.getAccountClient().getActivePackages(),
-               named(ProductPackageClientLiveTest.CLOUD_SERVER_PACKAGE_NAME)).getId();
-      ProductPackage productPackage = api.getProductPackageClient().getProductPackage(pkgId);
-
       VirtualGuest guest = VirtualGuest.builder().domain("jclouds.org").hostname(
                TEST_HOSTNAME_PREFIX + new Random().nextInt()).build();
 
-      Template template = templateBuilder.build();
-
       ProductOrder order = ProductOrder.builder()
-              .packageId(productPackage.getId())
+              .packageId(PACKAGE_ID)
               .quantity(1)
-              .location(template.getLocation().getId())
+              .location("37473")
               .useHourlyPricing(true)
-              .prices(getPrices(template, productPackage))
+              .prices(getPrices(1640, 2238, 13899, 272, 2142))
               .virtualGuests(guest).build();
 
       ProductOrderReceipt receipt = api().orderVirtualGuest(order);
@@ -111,28 +87,115 @@ public class VirtualGuestClientLiveTest extends BaseSoftLayerClientLiveTest {
    }
 
    @Test(groups = "live")
-   public void testVerifyOrder() {
+   public void testOrder() {
 
-      int pkgId = Iterables.find(api.getAccountClient().getReducedActivePackages(),
-              named(ProductPackageClientLiveTest.CLOUD_SERVER_PACKAGE_NAME)).getId();
-      ProductPackage productPackage = api.getProductPackageClient().getProductPackage(pkgId);
-
-      HardwareServer server = HardwareServer.builder().domain("jclouds.org").hostname(
-              TEST_HOSTNAME_PREFIX + new Random().nextInt()).build();
-
-      templateBuilder.locationId("37473"); // specify location because for some reason the default location is invalid.
-      Template template = templateBuilder.build();
+      VirtualGuest newGuest = VirtualGuest.builder().domain("jclouds.org").hostname(TEST_HOSTNAME_PREFIX + new Random().nextInt()).build();
 
       ProductOrder order = ProductOrder.builder()
-              .packageId(productPackage.getId())
+              .packageId(PACKAGE_ID)
               .quantity(1)
-              .location(template.getLocation().getId())
+              .location("37473")
               .useHourlyPricing(true)
-              .prices(getPrices(template, productPackage))
-              .hardwareServers(server).build();
+              .prices(getPrices(1640, 2238, 13899, 272, 13945))
+              .virtualGuests(newGuest).build();
 
       ProductOrder order2 = api().verifyVirtualGuestOrder(order);
       assertEquals(order.getPrices(), order2.getPrices());
+   }
+
+   /**
+    * We pass an image id that is mapped to an Ubuntu OS.
+    * But the Flex image global identifier we are using is built for CentOS only.
+    * In this case, the image identifier overrides the image id declaration.
+    *
+    * Make sure that the order accepted will in fact provision a CentOS Image.
+    */
+   @Test
+   public void testOrderWithImageTemplateGlobalIdentifierAndIncompatibleImageId() {
+
+      VirtualGuest newGuest = VirtualGuest.builder().domain("jclouds.org").hostname(TEST_HOSTNAME_PREFIX + new Random().nextInt()).build();
+
+      ProductOrder order = ProductOrder.builder()
+            .packageId(PACKAGE_ID)
+            .quantity(1)
+            .location("37473")
+            .useHourlyPricing(true)
+            .prices(getPrices(1640, 2238, 13899, 272, 2142))
+            .virtualGuests(newGuest)
+            .imageTemplateGlobalIdentifier("5ad7a379-5252-4c19-a02a-1a5882f4fcbf").build();
+
+      ProductOrder order2 = api().verifyVirtualGuestOrder(order);
+
+      ProductItemPrice centOSProductItemPrice = ProductItemPrice.builder().id(13945).build();
+      assertTrue(order2.getPrices().contains(centOSProductItemPrice));
+      assertEquals(order.getPrices().size(), order2.getPrices().size());
+
+   }
+
+   /**
+    * We dont pass an image id.
+    * But the Flex image global identifier we are using is built for CentOS only.
+    * In this case, the image identifier overrides the image id declaration.
+    *
+    * Make sure that the order accepted will in fact provision a CentOS Image.
+    */
+   @Test
+   public void testOrderWithImageTemplateGlobalIdentifierAndNoImageId() {
+
+      VirtualGuest newGuest = VirtualGuest.builder().domain("jclouds.org").hostname(TEST_HOSTNAME_PREFIX + new Random().nextInt()).build();
+
+      ProductOrder order = ProductOrder.builder()
+            .packageId(PACKAGE_ID)
+            .quantity(1)
+            .location("37473")
+            .useHourlyPricing(true)
+            .prices(getPrices(1640, 2238, 13899, 272))
+            .virtualGuests(newGuest)
+            .imageTemplateGlobalIdentifier("5ad7a379-5252-4c19-a02a-1a5882f4fcbf").build();
+
+      ProductOrder order2 = api().verifyVirtualGuestOrder(order);
+
+      ProductItemPrice centOSProductItemPrice = ProductItemPrice.builder().id(13945).build();
+      assertTrue(order2.getPrices().contains(centOSProductItemPrice));
+      assertEquals(order.getPrices().size() + 1, order2.getPrices().size());
+
+   }
+
+   @Test
+   public void testOrderWithImageTemplateGlobalIdentifier() {
+
+      VirtualGuest newGuest = VirtualGuest.builder().domain("jclouds.org").hostname(TEST_HOSTNAME_PREFIX + new Random().nextInt()).build();
+
+      ProductOrder order = ProductOrder.builder()
+            .packageId(PACKAGE_ID)
+            .quantity(1)
+            .location("37473")
+            .useHourlyPricing(true)
+            .prices(getPrices(1640, 2238, 13899, 272, 13945))
+            .virtualGuests(newGuest)
+            .imageTemplateGlobalIdentifier("5ad7a379-5252-4c19-a02a-1a5882f4fcbf").build();
+
+      ProductOrder order2 = api().verifyVirtualGuestOrder(order);
+      assertEquals(order.getPrices(), order2.getPrices());
+   }
+
+   @Test
+   public void testVerifyOrderWithImageTemplateId() {
+
+      VirtualGuest newGuest = VirtualGuest.builder().domain("jclouds.org").hostname(TEST_HOSTNAME_PREFIX + new Random().nextInt()).build();
+
+      ProductOrder order = ProductOrder.builder()
+            .packageId(PACKAGE_ID)
+            .quantity(1)
+            .location("37473")
+            .useHourlyPricing(true)
+            .prices(getPrices(1640, 2238, 13899, 272, 13945))
+            .virtualGuests(newGuest)
+            .imageTemplateId("93276").build();
+
+      ProductOrder order2 = api().verifyVirtualGuestOrder(order);
+      assertEquals(order.getPrices(), order2.getPrices());
+
    }
 
    private Iterable<ProductItemPrice> defaultPrices;
@@ -140,7 +203,6 @@ public class VirtualGuestClientLiveTest extends BaseSoftLayerClientLiveTest {
    @Override
    protected SoftLayerClient create(Properties props, Iterable<Module> modules) {
       Injector injector = newBuilder().modules(modules).overrides(props).buildInjector();
-      templateBuilder = injector.getInstance(TemplateBuilder.class);
       defaultPrices = injector.getInstance(Key.get(new TypeLiteral<Iterable<ProductItemPrice>>() {
       }));
       return injector.getInstance(SoftLayerClient.class);
@@ -170,21 +232,13 @@ public class VirtualGuestClientLiveTest extends BaseSoftLayerClientLiveTest {
       assert vg.getPrimaryIpAddress() != null : vg;
    }
 
-   private Iterable<ProductItemPrice> getPrices(Template template, ProductPackage productPackage) {
+
+   private Iterable<ProductItemPrice> getPrices(Integer... prices) {
       Builder<ProductItemPrice> result = ImmutableSet.builder();
-
-      int imageId = Integer.parseInt(template.getImage().getId());
-      result.add(ProductItemPrice.builder().id(imageId).build());
-
-      Iterable<String> hardwareIds = Splitter.on(",").split(template.getHardware().getId());
-      for (String hardwareId : hardwareIds) {
-         int id = Integer.parseInt(hardwareId);
-         result.add(ProductItemPrice.builder().id(id).build());
+      for (Integer price : prices) {
+         ProductItemPrice itemPrice = ProductItemPrice.builder().id(price).build();
+         result.add(itemPrice);
       }
-      float portSpeed = 10f;
-      ProductItem uplinkItem = find(productPackage.getItems(),
-              and(capacity(portSpeed), categoryCode("port_speed")));
-      result.add(get(uplinkItem.getPrices(), 0));
       result.addAll(defaultPrices);
       return result.build();
    }
